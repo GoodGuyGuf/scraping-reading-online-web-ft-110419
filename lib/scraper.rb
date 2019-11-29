654ade7c51ee0661efa2694c5008a21363bde821
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-29 03:10:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,18 +3,12 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
-puts doc.css(".headline-26OIBN")
+changes = doc.css(".headline-26OIBN").text
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css(".text-4GLMvr .title-oE5vT4")
 
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+course_names = text_about_courses.slice(0..3)
 
-courses.each do |course|
-  puts course.text.strip
-end
-
-p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0]
-
-p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
-
-p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
+course_names.each_with_index do |course, index|
+  puts "#{index+1}. #{course.text}"
+  end
